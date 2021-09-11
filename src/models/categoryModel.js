76dcd098f8d00a627d4a0c608eb5ca5d7c3b74e5
JRakin <juaidrakin1@gmail.com>
2021-09-11 17:47:07 +0600

    category added

---
+++ src/models/categoryModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const uuidv4 = require("uuid").v4;
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    min: 1,
+    max: 300,
+    unique: [true, "Category already exists"],
+  },
+  catId: {
+    type: String,
+    default: function () {
+      uuidv4();
+    },
+  },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = { Category };
